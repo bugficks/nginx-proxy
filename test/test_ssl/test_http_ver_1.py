4591613c7c8f173c70c58cc90c111c28da00980d
bugficks <bugficks@users.github.com>
2020-07-24 08:31:50 +0000

    fix copy/paste bug test_http_ver*.py

--- test/test_ssl/test_http_ver_1.py
+++ test/test_ssl/test_http_ver_1.py
@@ -6,8 +6,6 @@ docker_client = docker.from_env()
 def test_web1_http_ver_1(docker_compose, nginxproxy):
     sut_container = docker_client.containers.get("nginxproxy")
 
-    exit_code, http_version = sut_container.exec_run("curl -sIk -H 'Host: web1.nginx-proxy.tld/port' https://localhost -o/dev/null -w '%{http_version}'\n")
+    exit_code, http_version = sut_container.exec_run("curl -sIk -H 'Host: web1.nginx-proxy.tld' https://localhost -o/dev/null -w '%{http_version}'\n")
 
-    assert float(http_version) in [ 1.0, 1.1 ]
-
-
+    assert float(http_version) in [ 1.1, 1.0 ]
